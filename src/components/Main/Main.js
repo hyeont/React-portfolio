cd85c5c935f98f51d554a9a4fa74e38aed627773
hyeont <68629682+hyeont@users.noreply.github.com>
2022-10-06 23:33:22 +0900

    Add files via upload

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,15 @@
 import Header from "./Header";
+import './Main.css';
 
 const Main = () => {
   return (
-    <div>
+    <div className='main'>
       <Header />
+      <div className='intro'>
+        <div className='intro_name'>정현태</div>
+        <div className='intro_jht'>소개</div>
+        <button className='intro_btn'>더 알아보기</button>
+      </div>
     </div>
   );
 };
